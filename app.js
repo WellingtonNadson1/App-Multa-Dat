c2ca1f9f0e5d649c30e11d8afb91d7b60969ac33
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-06-14 22:26:25 -0300

    New refactor in the code, adjust in router e function division responsibilit

--- app.js
+++ app.js
@@ -2,29 +2,25 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
-// ROUTES
-const routes = require('./config/routes')
-
 // EXPRESS STATIC FILE (CSS)
 app.use(express.static('public'))
 
+// ROUTES IMPORT
+const router = require('./config/routes')
+
+
 app.set('view engine', 'ejs')
 
-// BodyParser 
-const bodyParser = require('body-parser')
-// const { urlencoded } = require('body-parser')
 
+// BODYPARSER 
+const bodyParser = require('body-parser')
 // CREATE APPLICATION/JSON PARSER
 app.use(bodyParser.json())
-
 // CREATE APPLICATION/X-WWW-FORM-URLENCOTED PARSER
 app.use(bodyParser.urlencoded({extended:false}))
 
-
 // ROUTERS
-app.use(routes)
-
+app.use(router)
 
 // LISTEN SERVER PORT 
 app.listen(port, () => console.log(`Rodando na porta: ${port}`))
---
+++ calculoMulta.js
@@ -0,0 +1,28 @@
+// MULTA
+function Multa(infra_leve, infra_media, infra_grave, infra_gravissima, fator_risco_empresa, fator_area_empresa){
+
+    // Valor Base Bombeiro Militar
+    const vbbm = 30.42
+
+    // Fator de Risco
+    const fator_risco = [1, 1.1, 1.2]
+
+    // Fator Área Edificada
+    const fator_area = [4, 8, 12, 16, 24, 30, 37, 43, 50, 56, 63, 69, 76, 83, 89, 94, 100 ]
+
+    const multa =
+    (
+        2.5 * parseInt(infra_leve) + 
+        3.5 * parseInt(infra_media) + 
+        5 * parseInt(infra_grave) + 
+        7 * parseInt(infra_gravissima)
+    )
+
+    * fator_risco[parseInt(fator_risco_empresa)] 
+    * fator_area[parseInt(fator_area_empresa)] 
+    * vbbm
+
+    return multa
+}
+
+module.exports = Multa
