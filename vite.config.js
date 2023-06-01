3717f928f1d08ec65cf4267ee32531e7fcd9a05b
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-06-01 10:16:50 -0300

    add file vite config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,14 @@
-const path = require('path')
-const { defineConfig } = require('vite')
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
-      fileName: (format) => `my-lib.${format}.js`
-    },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
+      input: {
+        main: resolve(__dirname, 'index.ejs'),
+        nested: resolve(__dirname, 'views/index.ejs'),
+      },
+    },
+  },
 })
